2a98fcf61331428ead3e4998a3b59532b1e68f06
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-12 22:55:55 +0000

    Task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 100):
-    if i < 10:
-        print("{}".format('0'), end="")
+for i in range(100):
     if i == 99:
         print("{}".format(i))
     else:
-        print("{}".format(i), end=", ")
+        print("{:02}".format(i), end=", ")
