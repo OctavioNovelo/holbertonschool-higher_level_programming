a1957e1413b2994ab6f86bcd95a1421681757fe0
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-12 22:52:13 +0000

    Task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 for i in range(00, 100):
     if i < 10:
         print("{}".format('0'), end="")
-    if i = 99:
+    if i == 99:
         print("{}".format(i))\
     else:
         print("{}".format(i), end=", ")
