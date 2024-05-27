ca68128a8964b4deb31afbb7ffdf34585ef8a7c3
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-27 21:09:54 +0000

    Task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while count < x:
+        while count <= x-1:
             print(my_list[count], end="")
             count += 1
     except IndexError:
