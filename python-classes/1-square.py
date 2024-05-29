ee66ee1a970d3ab2ed8d68dd029a92b6101c541c
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-29 18:28:40 +0000

    Task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,5 +2,11 @@
 """Write a class Square that defines a square"""
 
 class Square:
+    """Represent a Square"""
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
