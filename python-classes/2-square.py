04a87a9b33f0bba87b76e59c2559aec3e8333903
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-30 17:23:42 +0000

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a Square"""
+"""This module defines a class Square that represents a square."""
 
 
 class Square:
     """Represent a square.
-
+    
     Attributes:
         __size (int): The size of the square (private).
     """
-    
-    def __init__(self, size=0):
-         """Initialize a new Square.
 
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        
         Args:
             size (int): The size of the square (default is 0).
-
+        
         Raises:
             TypeError: If size is not an integer.
-            TypeValue: Size must be >= 0.
+            ValueError: If size is less than 0.
         """
-        self.__size = size
-        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__size = size
