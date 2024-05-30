b6733f6c7ea80d82205decb5910021b8cd108207
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-30 18:13:10 +0000

    Task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -24,3 +24,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+               int: The area of the square.
+        """
+        return self.__size ** 2
