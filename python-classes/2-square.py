62b6ade9062a0ee6fc1f61528b1a11fdb5d037d1
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-01 23:06:34 +0000

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,11 @@
 
 class Square:
      """defines class and
-     instantiates private instance attribute size with validation."""
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+     instantiates private instance attribute size with validation.
+     """
+     def __init__(self, size=0):
+          if not isinstance(size, int):
+               raise TypeError("size must be an integer")
+          if size < 0:
+               raise ValueError("size must be >= 0")
+          self.__size = size
