caa842cdded4fe22d73169be9b4507aed03830b5
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-02 00:58:25 +0000

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,6 @@
 
 class Square:
     """Represent a square.
-    
-    Attributes:
-      __size (int): The size of the square.
     """
     def __init__(self, size=0):
         if not isinstance(size, int):
