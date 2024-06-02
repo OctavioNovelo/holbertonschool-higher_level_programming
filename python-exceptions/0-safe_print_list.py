30d222450feb980668d6bd3f9f226dfc42f33c55
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-02 01:36:13 +0000

    Task1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[count], end="")
             count += 1
     except IndexError:
-        pass
+        print()
+        return count
     
     print()
-
     return count
