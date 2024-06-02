b25cebf3c93f78324ba9a2320e587a3c416127ad
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-02 01:16:46 +0000

    Task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Define a class Square with private instance attribute size."""
 
 
 class Square:
-    """Represent a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
-
+    """Represent a square."""
     def __init__(self, size=0):
         self.size = size
 
-
     def area(self):
-        """Calculate the area of the square.
-
-        Returns:
-               int: The area of the square.
-        """
+        """Return the area of square"""
         return self.__size ** 2
 
     @property
     def size(self):
-        """Get the size of the square.
-        
-        Returns:
-            int: The size of the square.
-        """
+        """Get the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Initialize a new Square.                                                                                                                                                  
-                                                                                                                                                                                     
-        Args:                                                                                                                                                                        
-            size (int): The size of the new square. It must be a non-negative integer.                                                                                               
-                                                                                                                                                                                     
-        Raises:                                                                                                                                                                      
-            TypeError: If size is not an integer.                                                                                                                                    
-            ValueError: If size is less than 0.                                                                                                                                      
-        """
+        """Set size of  Square."""         
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
