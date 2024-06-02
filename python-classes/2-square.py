df7267af93c6b829a9cd91c3a6804de3f802e63f
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-02 00:50:54 +0000

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""empty class"""
+"""Define a class Square with private instance attribute size."""
 
 
 class Square:
-    """empty class"""
+    """Represent a square.
+    Attributes:
+    __size (int): The size of the square.                                                                                                                                                                                                                     
+    """
     def __init__(self, size=0):
-        if type(size) is not int:
+        """Initialize a new Square.                                                                                                                                                                                                                                       Args:                                                                                                                                                                                                                                                                size (int): The size of the new square. It must be a non-negative integer.                                                                                                                                                                                   Raises:                                                                                                                                                                                                                                                              TypeError: If size is not an integer.                                                                                                                                                                                                                            ValueError: If size is less than 0.                                                                                                                                                                                                                         """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
