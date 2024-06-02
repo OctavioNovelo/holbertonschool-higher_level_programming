4c9e8c24bb22afe7df4ae23cfb5f9c222bee884d
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-02 00:44:18 +0000

    Task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3
-"""Define a class Square with private instance attribute size."""
+"""empty class"""
 
 
 class Square:
-    """Represent a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
-
+    """empty class"""
     def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square. It must be a non-negative integer.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
