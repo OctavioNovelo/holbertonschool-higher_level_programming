4de87f88b9a8c97ff645f458e386820524913db4
OctavioNovelo <octavionovelo22@gmail.com>
2024-06-03 03:56:42 +0000

    Task7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -3,9 +3,9 @@
 
 
 class Rectangle:
+    """Represent Rectangle."""
     number_of_instances = 0
     print_symbol = 0
-    """Represent Rectangle."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
