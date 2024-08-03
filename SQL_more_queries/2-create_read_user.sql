0f77539867426221e542e95eb680f85870e3cd90
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-03 15:59:36 -0600

    Correction

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,17 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+-- Create the database 'hbtn_0d_2' if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user 'user_0d_2' with a password, if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Revoke all privileges for this user on all databases to avoid undesired privileges
+REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_2'@'localhost';
+
+-- Revoke grant privileges to ensure they cannot grant privileges
+REVOKE GRANT OPTION ON *.* FROM 'user_0d_2'@'localhost';
+
+-- Only grant the SELECT privilege to 'user_0d_2' on the 'hbtn_0d_2' database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Immediately apply privilege changes
 FLUSH PRIVILEGES;
