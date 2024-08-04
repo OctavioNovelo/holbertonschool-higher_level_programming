3d01ee8cfcc19e6bcdf74f9497d29a89f5b2b3ef
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-04 06:24:20 +0000

    ...

---
+++ python-abc/test_circle_negative.py
@@ -0,0 +1,10 @@
+from task_01_duck_typing import Circle
+
+def test_circle_negative():
+    try:
+        circle_negative = Circle(radius=-5)
+    except ValueError as e:
+        print(e)
+
+if __name__ == "__main__":
+    test_circle_negative()
