26e73daf947128d6c82d308d6988b1cc49643ee8
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-04 10:06:43 +0000

    Task4

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import csv
+import sqlite3
 
 app = Flask(__name__)
 
@@ -18,6 +19,26 @@ def read_csv_file(file_path):
             products.append(row)
     return products
 
+def read_sqlite_database():
+    products = []
+    try:
+        conn = sqlite3.connect('products.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, name, category, price FROM Products')
+        rows = cursor.fetchall()
+        for row in rows:
+            products.append({
+                'id': row[0],
+                'name': row[1],
+                'category': row[2],
+                'price': row[3]
+            })
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return None
+    return products
+
 @app.route('/products')
 def products():
     source = request.args.get('source')
@@ -29,6 +50,10 @@ def products():
         products = read_json_file('products.json')
     elif source == 'csv':
         products = read_csv_file('products.csv')
+    elif source == 'sql':
+        products = read_sqlite_database()
+        if products is None:
+            error = "Database error"
     else:
         error = "Wrong source"
     
